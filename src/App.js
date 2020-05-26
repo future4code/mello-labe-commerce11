8f6df55139b2b43b3c3a0a0d8cd3b512487d28c4
Renan Takeshi <rtakeshi13@gmail.com>
2020-05-26 17:58:24 -0300

    GridProdutos sem estilo

--- src/App.js
+++ src/App.js
@@ -14,43 +14,43 @@ const produtos = [
   {
     id: 2,
     nome: "produto 2",
-    valor: 10,
+    valor: 20,
     imgUrl: "https://picsum.photos/200/200?a=2",
   },
   {
     id: 3,
     nome: "produto 3",
-    valor: 10,
+    valor: 30,
     imgUrl: "https://picsum.photos/200/200?a=3",
   },
   {
     id: 4,
     nome: "produto 4",
-    valor: 10,
+    valor: 40,
     imgUrl: "https://picsum.photos/200/200?a=4",
   },
   {
     id: 5,
     nome: "produto 5",
-    valor: 10,
+    valor: 50,
     imgUrl: "https://picsum.photos/200/200?a=5",
   },
   {
     id: 6,
     nome: "produto 6",
-    valor: 10,
+    valor: 60,
     imgUrl: "https://picsum.photos/200/200?a=6",
   },
   {
     id: 7,
     nome: "produto 7",
-    valor: 10,
+    valor: 70,
     imgUrl: "https://picsum.photos/200/200?a=7",
   },
   {
     id: 8,
     nome: "produto 8",
-    valor: 10,
+    valor: 80,
     imgUrl: "https://picsum.photos/200/200?a=8",
   },
 ];
@@ -58,19 +58,22 @@ const produtos = [
 const MainContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 3fr 1fr;
-  grid-template-rows: 80vh;
 `;
 
 class App extends React.Component {
   state = {
     produtos: produtos,
     carrinho: [],
+    ordenacao: "crescente",
   };
 
   adicionarAoCarrinho = (produto) => {
     const novoCarrinho = [...this.state.carrinho, produto];
-    this.setState({carrinho:novoCarrinho})
-    console.table(novoCarrinho)
+    this.setState({ carrinho: novoCarrinho });
+  };
+
+  mudarOrdenacao = (evento) => {
+    this.setState({ ordenacao: evento.target.value });
   };
 
   render() {
@@ -79,7 +82,9 @@ class App extends React.Component {
         <Filter></Filter>
         <GridProdutos
           produtos={this.state.produtos}
+          ordenacao={this.state.ordenacao}
           funcaoAdicionar={this.adicionarAoCarrinho}
+          funcaoOrdenacao={this.mudarOrdenacao}
         ></GridProdutos>
         <Carrinho></Carrinho>
       </MainContainer>
--- src/components/CardProduto/CardProduto.js
+++ src/components/CardProduto/CardProduto.js
@@ -6,11 +6,17 @@ const CardContainer = styled.div`
   flex-direction: column;
   padding: 5px;
   border: 1px dotted orange;
-  height: 400px;
+  margin-top: 5px;
 `;
 const Imagem = styled.img``;
 
-const BotaoAdicionar = styled.button``;
+const BotaoAdicionar = styled.button`
+  margin: 5px;
+`;
+
+const Descricao = styled.p`
+  margin: 5px;
+`
 
 function CardProduto(props) {
   const { produto, funcao } = props;
@@ -18,8 +24,8 @@ function CardProduto(props) {
   return (
     <CardContainer>
       <Imagem src={produto.imgUrl} />
-      <p>{produto.nome}</p>
-      <p>{produto.valor}</p>
+      <Descricao>{produto.nome}</Descricao>
+      <Descricao>R$: {produto.valor.toFixed(2)}</Descricao>
       <BotaoAdicionar onClick={funcao}>Adicionar ao Carrinho</BotaoAdicionar>
     </CardContainer>
   );
