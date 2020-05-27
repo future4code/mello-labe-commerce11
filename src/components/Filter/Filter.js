e888d59350a695822110f7912074c72e73b7b0ef
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-05-27 18:49:18 -0300

    Filtro-Estilizado

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,22 +2,39 @@ import React from "react";
 import styled from "styled-components";
 
 const BarraLateral = styled.div`
-    display:flex;
-    flex-direction: column;
-    border: 1px solid orange;
+  display:flex;
+  flex-direction: column;
+  padding: 5px;
+  font-family: Roboto;
+  border: 1px solid black;
+  margin: 30px 20px 0px 20px;
+
+`
+const SeçãoFiltro = styled.div`
+  margin: 10px 5px;
+`
+
+const Title = styled.h3`
+margin: 10px 5px;
 `
 
 
 function Filter(props) {
   const { funcaoMax, funcaoMin, funcaoRegex } = props;
   return <BarraLateral>
-    <h1>Pesquisar Produtos:</h1>
-    <label>Valor Máximo:</label>
-    <input placeholder="Maximo" type="number" onChange={funcaoMax} />
-    <label>Valor Mínimo':</label>
-    <input placeholder="Minimo" type="number" onChange={funcaoMin} />
-    <label>Buscar Produto:</label>
-    <input placeholder="Pesquisar" onChange={funcaoRegex} />
+    <Title>FILTROS</Title>
+    <SeçãoFiltro>
+     <label>Valor Máximo</label> <br />
+     <input type="number" onChange={funcaoMax} />
+    </SeçãoFiltro>
+    <SeçãoFiltro>
+     <label>Valor Mínimo</label> <br />
+     <input type="number" onChange={funcaoMin} />
+    </SeçãoFiltro>
+    <SeçãoFiltro>
+     <label>Buscar Produtos</label> <br />
+     <input onChange={funcaoRegex} />
+    </SeçãoFiltro>
   </BarraLateral>;
 }
 
